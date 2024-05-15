6bf2b7ecef57e0d742a39ae47548d8de5aea606f
Kayo Thyerre <kayothyerrework@gmail.com>
2024-05-15 01:05:27 -0300

    char selection finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,27 @@
 
-
-
 const characters = document.querySelectorAll('.character')
 
 characters.forEach((character) => {
     character.addEventListener('mouseenter', () => {
+
+        const idSelected = character.attributes.id.value;
+
+        if (idSelected  === 'ultron') return;
+
+
         const characterSelected = document.querySelector('.selected');
         characterSelected.classList.remove('selected');
            
         character.classList.add('selected');
 
-        const idSelected = character.attributes.id.value;
+        
         const imagePlayer1 = document.getElementById('p1-character');
         imagePlayer1.src = `./src/images/${idSelected}.png`;
 
-        const namePlayer1 = document.getElementById('name-player-1')
-        console.log(namePlayer1)
-        namePlayer1.innerHTML = 'personagem';
+        const namePlayer1 = document.getElementById('name-player-1');
+        const nameSelected = character.getAttribute('data-name');
+
+        namePlayer1.innerHTML = nameSelected;
+
     })
 })
